0f6e713c32bc0bafc87952b204943ed1943a0371
CaptainNatan <135669464+CaptainNatan@users.noreply.github.com>
2023-10-09 07:12:22 +0000

    Added 404-page

--- schedule.js
+++ schedule.js
@@ -15,87 +15,87 @@ ON CLICK EVENTS
 */
 
 //pop up menu to add class
-$(".addNew").on("click", function(){
-  
+$(".addNew").on("click", function() {
+
   $(".popUp").fadeToggle();
-  
+
 });
 
 //clicking to add class
-$(".addButton").on("click", function(){
-  
+$(".addButton").on("click", function() {
+
   var title = $(".nameInput").val();
-  
-  var checkboxValue = []; 
-  
-//retrives the values from selected checkboxes and pushes it into array
- $.each($("input:checked"), function(){            
-        checkboxValue.push($(this).val());
-   });
-  
+
+  var checkboxValue = [];
+
+  //retrives the values from selected checkboxes and pushes it into array
+  $.each($("input:checked"), function() {
+    checkboxValue.push($(this).val());
+  });
+
   var fromTimeHour = $("#fromTimeHour").val();
   var fromTimeHalf = $("#fromTimeHalf").val();
-  
+
   var toTimeHour = $("#toTimeHour").val();
   var toTimeHalf = $("#toTimeHalf").val();
-  
+
   var color = $(this).css('backgroundColor');
   // alert(color);
-  
+
   // alert(checkboxValue + " " + typeof checkboxValue[0] + " " + checkboxValue.length); //string 
-  
+
   //VALIDADE INPUTS
-  
-  if(title != "") {
+
+  if (title != "") {
     //create new div with class element and fadeout popup
-    
+
     //CREATE NEW ELEMENTS
-    for(var i = 0; i < checkboxValue.length; i++) {
-      
+    for (var i = 0; i < checkboxValue.length; i++) {
+
       createNew(title, fromTimeHour, fromTimeHalf, toTimeHour, toTimeHalf, Number(checkboxValue[i]), color);
-     
-    } 
-    
-      makeDeletable();
-      updateElements()
+
+    }
+
+    makeDeletable();
+    updateElements()
     $(".nameInput").val("");
     $(".popUp").fadeToggle();
-    
-    
+
+
   } else {
     //shake textbox
     //toggle
     shake();
     //untoggles after animation is done
-    setTimeout(shake,310);
+    setTimeout(shake, 310);
   }
-  
+
 });
 
- //clicking colors
+//clicking colors
 $(".color").on("click", function() {
-  
+
   let myColor = $(this).css('backgroundColor');
-  
+
   $(".addButton").css('backgroundColor', myColor);
-  
+
 });
 
-function shake () {
+function shake() {
   $(".nameInput").toggleClass("shake");
 }
 
 function createNew(title, fromTimeHour, fromTimeHalf, toTimeHour, toTimeHalf, weekDay, color) {
-  
-  
+
+
   var unit = 15;
   //create new element using title+weekday as ID
   var newElement = '<div id=' + title + weekDay + ' class="myClass ui-draggable ui-draggable-handle ui-resizable"><p class="title">' + title + '<i class="fa fa-trash-o" aria-hidden="true"></i></p><div class="ui-resizable-handle ui-resizable-s" style="z-index:90;"></div></div>';
-  
+
   //inserts it
   $(newElement).insertBefore(".tableTimes");
-   
-  
+
+
   //this is how we will control where the elements will appear on the grid
   //left for the day of the week
   //top for starting time
@@ -105,24 +105,24 @@ function createNew(title, fromTimeHour, fromTimeHalf, toTimeHour, toTimeHalf, we
     "top": getStartHour(fromTimeHour, fromTimeHalf),
     "height": getToHour(fromTimeHour, fromTimeHalf, toTimeHour, toTimeHalf),
     "background-color": "" + color,
-    
+
   });
-  
+
   updateElements();
 }
 
 function makeResizable() {
-  $( ".myClass" ).resizable({
-  handles: 's',
-  grid: [ 0, 15 ]
-});
+  $(".myClass").resizable({
+    handles: 's',
+    grid: [0, 15]
+  });
 }
 
 function makeDraggable() {
   $(".myClass").draggable({
-  containment: 'parent',
-    grid: [100,15]
-});
+    containment: 'parent',
+    grid: [100, 15]
+  });
   console.log("dragable");
 }
 
@@ -131,17 +131,17 @@ function makeDraggable() {
 function updateElements() {
   makeDraggable();
   makeResizable();
-  
+
 }
 
 function makeDeletable() {
-  
+
   $(".fa-trash-o").on("click", function() {
-    
-  $(this).parent().parent().remove();
-   
-  
-});
+
+    $(this).parent().parent().remove();
+
+
+  });
 }
 
 /*
@@ -151,81 +151,81 @@ NOTE: need to make sure inputs are valid on functions bellow and test more
 */
 
 function correctHour(toHour) {
-  
-        var result;
 
-        if(toHour < 7) {
-          result = 12 + Number(toHour);
-        } else {
-          result = toHour;
-        }
-        // alert(result);
-        return result;
-      }
+  var result;
+
+  if (toHour < 7) {
+    result = 12 + Number(toHour);
+  } else {
+    result = toHour;
+  }
+  // alert(result);
+  return result;
+}
 
 function getToHour(fromHour, fromHalf, toHour, toHalf) {
   //needs to handle 9am to 1pm types of entry
   var compensation;
-  
-  if(fromHalf == 30 && toHalf == 30) {
-    
+
+  if (fromHalf == 30 && toHalf == 30) {
+
     compensation = 0;
-    
-  } else if (fromHalf == 30 ){
-    
+
+  } else if (fromHalf == 30) {
+
     compensation = -15;
-    
-  } else if(toHalf == 30){
-    
+
+  } else if (toHalf == 30) {
+
     compensation = 15;
-    
+
   } else {
-    
+
     compensation = 0;
   }
-  
-    var correctedToHour = correctHour(toHour);
-    var correctedFromHour = correctHour(fromHour);
-    // alert(correctedHour);
-    return ((correctedToHour - correctedFromHour) * 30) + compensation;
-  
+
+  var correctedToHour = correctHour(toHour);
+  var correctedFromHour = correctHour(fromHour);
+  // alert(correctedHour);
+  return ((correctedToHour - correctedFromHour) * 30) + compensation;
+
 }
 
 function getStartHour(fromHour, fromHalf) {
-  
+
   let base = 110; //that gives 7am
   var unitHalf;
-  
+
   //this rounds down or up
   if (fromHalf >= 30) {
-    
+
     unitHalf = 15; //half an hour
-  
+
   } else {
-    
+
     unitHalf = 0;
   }
-  
-  
-  if(fromHour >= 7) {
-    
+
+
+  if (fromHour >= 7) {
+
     return base + ((fromHour - 7) * 30) + unitHalf;
-    
+
   } else {
     //260 is the base for anything after 12
-    return  260 + (fromHour * 30) + unitHalf;
-    
+    return 260 + (fromHour * 30) + unitHalf;
+
   }
-  
+
 };
 
-function validateInput (number1, number2) {
+function validateInput(number1, number2) {
   //tittle cant be an empty string
   //fromHour > toHour
   //array of weekdays is not empty
   //color with r,g or b lower than 130 makes color of text white
-  
-  
+
+
 }
 
 
